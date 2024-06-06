61652acf78732a3574adb786e466ffbb36d6110a
ardRiriy <hynnkgw27@gmail.com>
2024-06-07 06:39:48 +0900

    fix ignore

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+
+const supabase =
+    createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
+    );
+
+const App = () => <Auth supabaseClient={supabase} />
